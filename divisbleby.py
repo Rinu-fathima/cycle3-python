ab2d1c9bc630f1b66018718c12a13380e558ba75
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-04 11:18:45 +0530

    Update divisbleby.py

--- divisbleby.py
+++ divisbleby.py
@@ -1,6 +1,6 @@
 upperlimit=int(input("Enter the upper limit :"))
 sum = 0
 for num in range (1,upperlimit):
-if num % 6 == 0 and num % 4 != 0:
-sum += num
+  if num % 6 == 0 and num % 4 != 0:
+    sum += num
 print(f"The Sum of all integers divisible by 6 but not by 4 are:{sum} ")
