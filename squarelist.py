685dd77439b9cf03dda25a70336352a422526939
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-04 10:53:06 +0530

    Create squarelist.py

---
+++ squarelist.py
@@ -0,0 +1,23 @@
+import math
+start=int(input("Enter the starting range of a four digit number:"))
+end=int(input("Enter the ending range of a four digit number:"))
+squareevenlist=[]
+for i in range(start,end+1):
+if 1000<=i<=9999:
+sqrtno=math.isqrt(i)
+if sqrtno*sqrtno==i:
+isalleven=True
+temp=i
+while temp&gt;0:
+digit=temp%10
+if digit%2!=0:
+isalleven=False
+break
+temp//=10
+if isalleven:
+squareevenlist.append(i)
+else:
+print("Enter four digit no range")
+break
+if squareevenlist!=[]:
+print(f"Four digit perect square with all even digits:{squareevenlist}")
