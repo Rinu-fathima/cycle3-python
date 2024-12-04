3ac4887405f00848faa92ecb50450e1c221dc086
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-04 11:23:44 +0530

    Update sumoflist.py

--- sumoflist.py
+++ sumoflist.py
@@ -2,8 +2,8 @@ n=int(input("Enter a no of terms:"))
 sum=0
 num=[]
 for i in range(n):
-n1=int(input("Enter numbers"))
-num.append(n1)
+  n1=int(input("Enter numbers"))
+  num.append(n1)
 for i in num:
-sum+=i
+  sum+=i
 print(f"Sum of list:{sum}")
