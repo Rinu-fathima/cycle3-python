109de5cb92893bf40ce85190846bf30c2d32e8fa
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-04 11:20:21 +0530

    Update factor.py

--- factor.py
+++ factor.py
@@ -2,7 +2,7 @@ number=int(input("Enter a number to find its factors:"))
 i=1
 factor=[]
 while i &lt;= number:
-if number % i == 0:
-factor.append(i)
-i += 1
+  if number % i == 0:
+    factor.append(i)
+  i += 1
 print(f"The factors of {number} are:{factor}")
