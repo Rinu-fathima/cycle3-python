4b45076780152f2cc4882823a725b89506a1b3a4
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-04 11:06:07 +0530

    Create amstrong.py

---
+++ amstrong.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number:"))
+temp = num
+sum = 0
+while num > 0:
+digit = num % 10
+sum += digit ** 3
+num //= 10
+if sum == temp:
+print(f"{temp} is an armstrong number.")
+else:
+print(f"{temp} is not an armstrong number")
