0dd54a06cb7c5d133f602c7864a5a55e29100c91
Rinu-fathima <24mp2291@rit.ac.in>
2024-12-04 11:25:23 +0530

    Update sumofprime.py

--- sumofprime.py
+++ sumofprime.py
@@ -1,13 +1,13 @@
 upper_limit=int(input("enter the upperlimit:"))
 for num in range (1,upper_limit+1):
-sum=0
-for digit in str(num):
-sum+=int(digit)
-if sum&gt;1:
-is_prime=True
-for i in range (2,int(sum**0.5)+1):
-if sum % i==0:
-is_prime=False
-break
-if is_prime:
-print("the sum of digit of{num}:{sum},is a prime")
+  sum=0
+  for digit in str(num):
+    sum+=int(digit)
+  if sum>1:
+    is_prime=True
+    for i in range (2,int(sum**0.5)+1):
+      if sum % i==0:
+        is_prime=False
+        break
+    if is_prime:
+    print("the sum of digit of{num}:{sum},is a prime")
